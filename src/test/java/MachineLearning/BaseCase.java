1e258e1766203134f297d6c649da69d06a5d2a83
ian <ianshine@hotmail.com>
2018-06-20 16:06:59 +0800

    更新测试脚本

---
+++ src/test/java/MachineLearning/BaseCase.java
@@ -0,0 +1,11 @@
+package MachineLearning;
+
+import com.springBoot.TestFrame.httpUtil.HttpRequestUtil;
+import com.springBoot.TestFrame.util.GetEvnAddress;
+
+public class BaseCase {
+
+    GetEvnAddress getEvnAddress = new GetEvnAddress();
+    HttpRequestUtil httpRequestUtil = new HttpRequestUtil();
+
+}
--- src/test/java/MachineLearning/DataPreview.java
+++ src/test/java/MachineLearning/DataPreview.java
@@ -1,20 +1,22 @@
 package MachineLearning;
 
 import com.alibaba.fastjson.JSON;
-import com.springBoot.TestFrame.httpUtil.HttpRequestUtil;
-import com.springBoot.TestFrame.util.GetEvnAddress;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.log4j.Logger;
 import org.junit.jupiter.api.Test;
-
 import java.util.HashMap;
 import java.util.Map;
 
 /**
  * 数据预览接口
  */
-public class DataPreview extends HttpRequestUtil{
-    GetEvnAddress getEvnAddress = new GetEvnAddress();
+public class DataPreview extends BaseCase{
 
-        String DataPreviewUrl = getEvnAddress.getDataPreviewUrl();
+    String DataPreviewUrl = getEvnAddress.getDataPreviewUrl();
+
+//    public static Logger logger = Logger.getLogger(DataPreview.class);
+    private static final Log log = LogFactory.getLog(DataPreview.class);
 
     @Test
     public void DataPreviewTest(){
@@ -30,8 +32,10 @@ public class DataPreview extends HttpRequestUtil{
         dbMap.put("rowDelimiter","自动识别处理");
 
         String DataJson = JSON.toJSONString(dbMap);
-        System.out.println(DataJson);
-//        sendGet("DataPreviewUrl","json");
+//        System.out.println(DataJson);
+        log.info(DataJson);
+
+//        httpRequestUtil.sendPost("DataPreviewUrl",DataJson);
 
     }
 
